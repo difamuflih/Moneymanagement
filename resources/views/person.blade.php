6f7453216d3dd53fc6d746e855a06361ebb22a09
difamuflih <difamuflih@gmail.com>
2024-12-30 06:58:26 +0700

    install flowbite

--- resources/views/person.blade.php
+++ resources/views/person.blade.php
@@ -1,1 +1,12 @@
-ini adalah halaman manajemen person
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body>
+  <h1 class="text-3xl font-bold text-red-700">
+    Ini adalah halaman manajemen money person
+  </h1>
+</body>
+</html>
