c8624fd87d48e28a3a9790c2d511286b2c16fc19
difamuflih <difamuflih@gmail.com>
2024-12-30 06:31:18 +0700

    install tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,12 @@
-ini adalah halaman home
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl font-bold text-red-700">
+    Ini adalah halaman home
+  </h1>
+</body>
+</html>
